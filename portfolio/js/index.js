b292cb97599cf91b3ece855d2e96fbc0d3954fff
Ivan Gricyan <gricyan82@gmail.com>
2022-01-20 00:43:25 +0100

    feat: add menu openning without overflow

--- portfolio/js/index.js
+++ portfolio/js/index.js
@@ -1,12 +1,5 @@
 $(document).ready(function() {
-  $(".hamburger").click(function() {
-    $(this).toggleClass("is-active");
+  $('.hamburger').click(function(event) {
+    $('.hamburger, .nav').toggleClass('is-active');
   });
 });
-
-// $(document).ready(function() {
-//   $('.header_burger, .header_list').click(function(event) {
-//       $('.header_burger, .header_menu').toggleClass('active');
-//       $('body').toggleClass('lock');
-//   });
-// });
