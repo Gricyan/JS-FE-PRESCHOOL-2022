08ebc465ac0dfc3be5847c157e0b6e7c34741dab
Ivan Gricyan <gricyan82@gmail.com>
2022-01-29 03:08:05 +0100

    fix preloader loading speed

--- portfolio/js/preloader.js
+++ portfolio/js/preloader.js
@@ -3,6 +3,6 @@
 setTimeout(function() {
   document.querySelector("html").classList.add("loader")
   document.querySelector("html").classList.remove("loader")
-  document.querySelector(".loader").style.display = "none"
   document.querySelector(".loader").style.transition = "all 1.5s ease"
+  document.querySelector(".loader").style.display = "none"
 }, 3500);
