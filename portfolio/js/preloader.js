ebf379003a209c32be3cd80f15ade05eef42ddd7
Ivan Gricyan <gricyan82@gmail.com>
2022-01-30 23:14:47 +0100

    fix preloader time

--- portfolio/js/preloader.js
+++ portfolio/js/preloader.js
@@ -3,6 +3,10 @@
 setTimeout(function() {
   document.querySelector("html").classList.add("loader")
   document.querySelector("html").classList.remove("loader")
-  document.querySelector(".loader").style.transition = "all 1.5s ease"
+  document.querySelector(".loader").style.transition = "all 1s ease"
+  document.querySelector(".loader").style.opacity = "0"
+}, 2000);
+
+setTimeout(function() {
   document.querySelector(".loader").style.display = "none"
-}, 3500);
+}, 3000);
