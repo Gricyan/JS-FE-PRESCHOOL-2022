2f04e3b3c384ebf2e17731eff7a962f982a35ad3
Ivan Gricyan <gricyan82@gmail.com>
2022-02-14 05:41:50 +0100

    feat: add movie content

--- movie-app/js/script.js
+++ movie-app/js/script.js
@@ -12,5 +12,49 @@ function sendRequest(method, url) {
 }
 
 sendRequest('GET', requestUrl)
-  .then(data => console.log(data))
+  .then((data) => {
+
+
+
+    console.log(data)
+
+    for (let i = 0; i < 15; i++) {
+
+      if (data.results[i].backdrop_path === null) {
+        continue;
+      } else {
+
+        const divItemElement = document.createElement("div");
+        divItemElement.classList.add("movie-item");
+
+        const imgPoster = document.createElement("img");
+        imgPoster.src = `https://www.themoviedb.org/t/p/w440_and_h660_face${data.results[i].poster_path}`
+
+        const divMovieInfo = document.createElement("div");
+        divMovieInfo.classList.add("movie-info");
+        const movieHeader = document.createElement("h2");
+        const movieHeaderName = document.createTextNode(data.results[i].title);
+        const movieRate = document.createElement("span");
+        const movieRateNum = document.createTextNode(data.results[i].vote_average);
+
+        const divMovieOverview = document.createElement("div");
+        divMovieOverview.classList.add("movie-overview");
+        const movieOverviewText = document.createTextNode(data.results[i].overview);
+
+        divItemElement.append(imgPoster);
+        divItemElement.append(divMovieInfo);
+        divMovieInfo.append(movieHeader);
+        movieHeader.append(movieHeaderName);
+        divMovieInfo.append(movieRate);
+        movieRate.append(movieRateNum);
+        divItemElement.append(divMovieOverview);
+        divMovieOverview.innerHTML = "<h3>Overview</h3>";
+        divMovieOverview.append(movieOverviewText);
+
+        document.querySelector(".movies").append(divItemElement);
+
+      };
+
+    }
+  })
   .catch(err => console.error(err))
