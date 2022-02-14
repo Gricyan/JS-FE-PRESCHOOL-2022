6addd5e27e309c47ebd2018dfa081a944e165f63
Ivan Gricyan <gricyan82@gmail.com>
2022-02-14 02:58:51 +0100

    feat: add header, footer, logo and bg

--- movie-app/js/script.js
+++ movie-app/js/script.js
@@ -1,1 +1,16 @@
-// ***
+const requestUrl = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query=spring&page=1'
+
+// GET method ---------------------------------------
+
+function sendRequest(method, url) {
+
+  return fetch(url).then(response => {
+    //return response.text()
+    return response.json()
+  })
+
+}
+
+sendRequest('GET', requestUrl)
+  .then(data => console.log(data))
+  .catch(err => console.error(err))
