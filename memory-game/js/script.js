fa31f1991c4f3cdcc1ceff370da5498725452641
Ivan Gricyan <gricyan82@gmail.com>
2022-02-21 19:17:53 +0100

    feat: add userName input and best score

--- memory-game/js/script.js
+++ memory-game/js/script.js
@@ -1,3 +1,5 @@
+/* Create cards list */
+
 const cardsList = document.querySelector('.cards')
 const imagesArray = [11, 12, 21, 22, 31, 32, 41, 42, 51, 52, 61, 62, 71, 72, 81, 82]
 
@@ -14,19 +16,24 @@ imagesArray.forEach((imageNum) => {
   cardsList.appendChild(cardsItem);
 })
 
-
-
+/* Flipping cards and count moves*/
 
 const cards = document.querySelectorAll('.card')
+const movesShow = document.querySelector('.counter')
+
 let cardOne, cardTwo
 let isFalse = false
 let matchedCards = 0
+let counter = 1
 
-
-
+function countMoves() {
+  movesShow.innerHTML = counter++
+}
 
 function flipCard(event) {
   let clickedCard = event.target
+
+  countMoves()
 
   if (clickedCard !== cardOne && !isFalse) {
     clickedCard.classList.add('flip')
@@ -45,22 +52,26 @@ function flipCard(event) {
   }
 }
 
+/* Match cards */
+
 function matchCars(img1, img2) {
 
   if (img1 === img2) {
 
-
-
     matchedCards++
     if (matchedCards == 8) {
+
+      updateScore(counter)
+
       setTimeout(() => {
+        movesShow.innerHTML = 0
+        counter = 1
         return shuffleCard()
       }, 1200)
     }
 
     cardOne.removeEventListener('click', flipCard)
     cardTwo.removeEventListener('click', flipCard)
-
 
     setTimeout(() => {
       cardOne.classList.add('match')
@@ -87,6 +98,8 @@ function matchCars(img1, img2) {
   }
 }
 
+/* Shuffle cards for new games */
+
 function shuffleCard() {
   matchedCards = 0
   cardOne = cardTwo = ''
@@ -108,14 +121,49 @@ function shuffleCard() {
 
 shuffleCard()
 
+/* Cards click */
+
 cards.forEach(card => {
   card.addEventListener('click', flipCard)
 })
 
+/* Score */
+
+const usersScore = {
+  name: '',
+  moves: []
+}
+
+const userNameShow = document.querySelector('.user-name')
+const button = document.querySelector('.btn');
+const entranceScreen = document.querySelector('.entrance');
 
 
+function createUser() {
+  button.onclick = (event) => {
 
+    event.preventDefault();
+    let userName = document.getElementById('userNameInput').value
 
+    if (userName != null) {
+      userNameShow.innerHTML = userName
+    }
 
+    usersScore.name = userName
 
-//document.querySelector('.counter').innerHTML = counter
+    entranceScreen.classList.add('hide')
+    setTimeout(() => {
+      entranceScreen.style.display = 'none'
+    }, 500)
+
+  }
+}
+
+createUser()
+
+function updateScore(counter) {
+  usersScore.moves.push(counter)
+  let minScore = Math.min.apply(Math, usersScore.moves)
+  let bestResultShow = document.querySelector('.best-result')
+  bestResultShow.innerHTML = minScore
+}
